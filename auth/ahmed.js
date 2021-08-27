35e9c894aaa0de5d2f54172d89d7cf5ac3653539
elmahdy <elmahdy30@gmail.com>
2021-08-27 15:57:06 +0200

    add some commands

--- auth/ahmed.js
+++ auth/ahmed.js
@@ -1,1 +1,13 @@
 console.log('ahmed')
+git remote add {alias} {url}
+alias = origin
+{url} = dest of repo
+
+git push -u
+-u = --set-upstream
+
+git push
+
+
+git clone -> url
+ git pull -> update your local folder 
