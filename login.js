79504a9f31343c5ec7dc1c84bcb99501eae27345
elmahdy <elmahdy30@gmail.com>
2021-08-28 12:39:37 +0200

    validate, send email

--- login.js
+++ login.js
@@ -4,9 +4,12 @@ function login(username, password){
   // ajax post reques
 }
 function login_by_email(email, password){
-  
+
 }
 
 function login_by_totp(username, totp){
   //totp
+}
+function validate_totp(totp){
+  // validate
 }
